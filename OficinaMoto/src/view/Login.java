3ec1903296354fd4dba216990f983f53059913b3
Ismael de Sousa <ismaelmouracontato@outlook.com>
2021-11-03 20:50:58 -0300

    Login

--- OficinaMoto/src/view/Login.java
+++ OficinaMoto/src/view/Login.java
@@ -10,6 +10,7 @@ import javax.swing.border.EmptyBorder;
 import model.DAO;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Toolkit;
@@ -20,12 +21,16 @@ import javax.swing.JPasswordField;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings({ "unused", "serial" })
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtUsuario;
+	private JTextField txtLogin;
 	private JPasswordField txtSenha;
 	private JLabel lblStatus;
 
@@ -65,21 +70,26 @@ public class Login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Usu\u00E1rio");
+		JLabel lblNewLabel = new JLabel("Login");
 		lblNewLabel.setBounds(88, 60, 46, 14);
 		contentPane.add(lblNewLabel);
 
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(144, 57, 200, 20);
-		contentPane.add(txtUsuario);
-		txtUsuario.setColumns(10);
+		txtLogin = new JTextField();
+		txtLogin.setBounds(144, 57, 200, 20);
+		contentPane.add(txtLogin);
+		txtLogin.setColumns(10);
 
 		JLabel lblNewLabel_1 = new JLabel("Senha");
 		lblNewLabel_1.setBounds(88, 85, 46, 14);
 		contentPane.add(lblNewLabel_1);
 
 		JButton btnEntrar = new JButton("Entrar");
-		btnEntrar.setBounds(88, 171, 89, 23);
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logar();
+			}
+		});
+		btnEntrar.setBounds(88, 162, 75, 32);
 		contentPane.add(btnEntrar);
 
 		lblStatus = new JLabel("");
@@ -112,4 +122,42 @@ public class Login extends JFrame {
 			System.out.println(e);
 		}
 	}// fim do método status()
+	
+	DAO dao = new DAO();
+	/**
+	 * Método responsável pela autenticação do usuário
+	 */
+	private void logar() {
+		if (txtLogin.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Preencha o campo Login", "Atenção!", JOptionPane.WARNING_MESSAGE);
+		} else if (txtSenha.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Preencha o campo Senha", "Atenção!", JOptionPane.WARNING_MESSAGE);
+		} else {
+			try {
+				String read = "select * from usuarios where login=? and senha=md5(?)";
+				Connection con = dao.conectar();
+				PreparedStatement pst = con.prepareStatement(read);
+				pst.setString(1, txtLogin.getText());
+				pst.setString(2, txtSenha.getText());
+				// a linha abaixo executa a query(instrução SQL) armazenando o resultado no
+				// objeto rs
+				ResultSet rs = pst.executeQuery();
+				// se existir o login e senha correspondente
+				if (rs.next()) {
+					// ir para a área do cliente
+					Oficina oficina = new Oficina();
+					oficina.setVisible(true);
+					// finalizar o JFrame
+					this.dispose();
+				} else {
+					JOptionPane.showMessageDialog(null, "Login e/ou senha inválido(s)", "Atenção!",
+							JOptionPane.WARNING_MESSAGE);
+				}
+				con.close();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		}
+	}
 }
